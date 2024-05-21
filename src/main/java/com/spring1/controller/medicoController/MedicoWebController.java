c6d66c5c6edb7eafd250f493b3e3da5658b0b7b9
Felipe Talini <felipetalini5@gmail.com>
2024-05-21 15:48:54 -0300

    Add web controller, termino da medapicontroller, e criacao da controller

---
+++ src/main/java/com/spring1/controller/medicoController/MedicoWebController.java
@@ -0,0 +1,15 @@
+package com.spring1.controller.medicoController;
+import com.spring1.model.Usuario;
+import com.spring1.repository.UsuarioRepository;
+import com.spring1.service.UsuarioService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class MedicoWebController {
+}
