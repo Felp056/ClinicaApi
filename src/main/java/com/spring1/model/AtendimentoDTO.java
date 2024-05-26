1bb0df178d6808d7b4e2e372a75728f29a6ba337
DelGrau <alanhipermassa@gmail.com>
2024-05-25 22:33:37 -0300

    Criada classe de transferencia de informações

---
+++ src/main/java/com/spring1/model/AtendimentoDTO.java
@@ -0,0 +1,12 @@
+package com.spring1.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AtendimentoDTO {
+    private String anamnese;
+    private String medico;
+    private String paciente;
+}
