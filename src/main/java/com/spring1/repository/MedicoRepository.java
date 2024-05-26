2f834d8e9e7005cfbe4cb6ab9f0e872edccd968e
DelGrau <alanhipermassa@gmail.com>
2024-05-25 22:35:34 -0300

    Adicionados metodos de buscar por ID e por Nome

--- src/main/java/com/spring1/repository/MedicoRepository.java
+++ src/main/java/com/spring1/repository/MedicoRepository.java
@@ -1,8 +1,12 @@
 package com.spring1.repository;
 
 import com.spring1.model.Medico;
+import com.spring1.service.MedicoService;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MedicoRepository extends JpaRepository<Medico, Integer> {
-    Medico findMedicoById(Integer id);
+    Medico findMedicoById(int id);
+    Medico findMedicoByNome(String nome);
 }
--- src/main/java/com/spring1/repository/PacienteRepository.java
+++ src/main/java/com/spring1/repository/PacienteRepository.java
@@ -1,7 +1,10 @@
 package com.spring1.repository;
 
 import com.spring1.model.Paciente;
+import com.spring1.service.PacienteService;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+    public Paciente findPacienteById(Long id);
+    public Paciente findPacienteByNome(String nome);
 }
